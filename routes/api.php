010687fd1b2e5d53bf6100aaa2e1cae59e7a0b2a
Kreasi Putra Subing <ralfian096@gmail.com>
2024-06-13 04:09:35 +0700

    Fix API routing

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,11 @@ Route::get('endpoint2', [RESTV1\Endpoints\Endpoint2::class, 'index'])
     ->middleware(['auth:bearer']);
 
 ## Custom 404
-Route::any('{any}', function ($any) {
+$custom404 = function () {
     return (new Errors)
         ->setInternal(false)
         ->setMessage(404, "Endpoint or HTTP method not available")
         ->sendError();
-});
+};
+Route::any('/', $custom404);
+Route::any('{any}', $custom404);
